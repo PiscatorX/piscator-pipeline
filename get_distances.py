6ee4bf35d0e51e0a55495b5f86cf599dbd24399b
Drew X <drewxdvst@outlook.com>
2017-11-28 22:26:03 +0200

    Fix the sorting of names according to the number of sequences in clusters

--- get_distances.py
+++ get_distances.py
@@ -26,20 +26,16 @@ class GetDistances():
         args = parser.parse_args()
         self.threads = args.threads
         self.working_dir = args.working_dir
-        #print self.working_dir
-        self.file_patt = os.path.join(self.working_dir, '*.cd_hits.clstr')
+        self.file_patt = os.path.join(self.working_dir, '*.cd_hits')
         self.plot_type = args.plot_type
-        args = parser.parse_args()
-        
         self.dist_list  = []
         self.distance_arrays = {}
-
+        self.data_len = {}
         
        
     def init_get_dist(self):
 
-        file_patt  = self.file_patt.replace('.clstr','')
-        map(self.get_dist, glob.iglob(file_patt))
+        map(self.get_dist, glob.iglob(self.file_patt))
          
         
         
@@ -58,7 +54,7 @@ class GetDistances():
         #     return
         self.dist_list.append(dist_fname.replace('dist','dat'))
         self.parse_dist(dist_fname)
-
+        
 
         
     def parse_dist(self, dist_fname):
@@ -67,9 +63,9 @@ class GetDistances():
         split_row  =  lambda  row: row.split()
         seq_ids = {}
         
+        
         try:
             with open(dist_fname) as dist_file:
-                print 'Parse',dist_fname
                 n = int(dist_file.next())    
                 data_matrix = np.zeros((n,n))
                 for i, row in enumerate(dist_file, 0):
@@ -77,9 +73,10 @@ class GetDistances():
                 dist_matrix = data_matrix[np.triu_indices(n)]
                 n_array = len(dist_matrix)
                 if (n_array > 100000):
-                    dist_matrix = np.random.choice(dist_matrix, 100000,  replace=True)
-                    print "***n_array**",n_array,dist_fname
+                    dist_matrix = np.random.choice(dist_matrix, 100000,  replace=True)            
                 self.distance_arrays[dist_fname] = dist_matrix
+                print '>>>',dist_fname
+                self.data_len[dist_fname] =  i
         except IOError as err:
             print err
             return
@@ -89,10 +86,9 @@ class GetDistances():
 
         
         self.f_list = self.distance_arrays.keys()
-        
-        print 'Compile data',self.f_list
-        
-        cluster_ids = [cluster_fname.split('r_')[-1].replace('.dist','') for cluster_fname in self.f_list ]
+        strip_fname = lambda fname: fname.split('r_')[-1].replace('.dist','') 
+        lens = {}
+        cluster_ids = [ strip_fname(cluster_fname)  for cluster_fname in self.f_list ]
         self.cluster_fnames = {}
         
         for perc_id  in set(cluster_ids):
@@ -106,27 +102,19 @@ class GetDistances():
                 continue
             cluster_data = {}
             for f_name in self.f_list:
-                
-                cluster_data[os.path.basename(f_name.rsplit('_', 1)[0])] = self.distance_arrays[f_name]
-            self.Init_plot(cluster_data, plt_fname) 
+                tag = os.path.basename(f_name.rsplit('_', 1)[0])
+                cluster_data[tag] = self.distance_arrays[f_name]
+                lens[tag] = self.data_len[f_name]
+            self.Init_plot(cluster_data, plt_fname, lens) 
+            
         
-        
-    def Init_plot(self, cluster_data, plt_fname, top = 3):
-
-        
-        if not cluster_data:
-            return
-        
-        cluster_data = cluster_data
-        data_len = dict((name, len(data)) for name, data in cluster_data.items() )
-
-        
-        labels = sorted(data_len, key=data_len.get, reverse=True)
+    def Init_plot(self, cluster_data, plt_fname, lens, top = 3):
+     
+        labels = sorted(lens, key=lens.get, reverse=True)
         dist_data = [ cluster_data[k] for k in labels ]
-        locations = dict((i,j) for j,i in enumerate(labels,1))    
-
-        for x in cluster_data.values():
-            print x.shape
+        locations = dict((i,j) for j,i in enumerate(labels,1))   
+        for k,v in cluster_data.items():
+            print k ,v.shape
         merged_data  = np.concatenate(dist_data)
         y_max, y_min = map(lambda func: func(merged_data), [np.amax, np.amin])
         y_max = y_max + 0.075 * top
@@ -148,11 +136,7 @@ class GetDistances():
         linestyle='-.',     # use dashed lines
         which='major',      # only major ticks
         color='lightgrey',  # line colour
-        alpha=0.7)          # make lines semi-translucent
-
-        # for box, colour in zip(plot['boxes'], box_colors):
-        #     plt.setp(box, color='turquoise', linewidth=0.5, facecolor='blue')
-        
+        alpha=0.7)          # make lines semi-translucent   
 
         axs.set_xticklabels(labels, fontsize=10, rotation=-20, ha='left')
         axs.yaxis.set_tick_params(labelsize=10)
@@ -185,13 +169,13 @@ class GetDistances():
         for flier in plot['fliers']:
             flier.set(marker='o', color='#e7298a', alpha=0.5)
 
-                
-        if data_len > 2:
-            top = data_len if top < 3 else top
+        n_clusters = len(self.data_len)    
+        if n_clusters > 2:
+            top = n_clusters if top < 3 else top
             for i,j in it.combinations(top, 2):
-                print i,j
+                print "({},{})".format(i,j),
                 H_value, p_value = mstats.kruskalwallis(cluster_data[i], cluster_data[j])
-                print 'next'
+                print 'H_value: {}, p_value: {}'.format(H_value, p_value)
                 y_max = y_max - 0.075
                 i = locations[i]
                 j = locations[j]
@@ -204,12 +188,11 @@ class GetDistances():
                                 linewidth=1,
                                 color='blue'))
                 axs.text((i + j)/2.0 ,
-                    y_max+0.065,
+                    y_max+0.04,
                     self.get_stars(p_value),
                     horizontalalignment='center',
                     verticalalignment='center')
 
-                
         plt.savefig("{}".format(plt_fname), bbox_inches='tight')
         plt.close()
 
